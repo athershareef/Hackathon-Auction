ce9af75be5834920940cc730541ef8ea2cc9ea21
Mohammed Ather Ahmed Shareef <mxs166331@utdallas.edu>
2017-12-17 10:00:20 -0600

    Angular Code
    Angular Code UI

---
+++ AuctionAngular/src/app/model/item.ts
@@ -0,0 +1,30 @@
+export class Item {
+  public itemId: number;
+
+  public partNo: number;
+
+  public name: string;
+
+  public description: string;
+
+  public quantity: number;
+
+  public imageUrl: string;
+
+  public cond: string;
+
+  public giftUser: string;
+
+
+  // constructor(name: string, category: string, price: number, imageUrl: string, brand: string, agegroup: string, itemId?: number) {
+  //   this.itemId = itemId;
+  //   this.name = name;
+  //   this.category = category;
+  //   this.price = price;
+  //   this.imageUrl = imageUrl;
+  //   this.brand = brand;
+  //   this.agegroup = agegroup;
+  // }
+
+
+}
